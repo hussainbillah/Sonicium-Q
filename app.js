15d4dbf791eda7fe734daf0fa5602bd3d3bef3d5
hussainbillah <wnnbdonline@gmail.com>
2025-04-12 18:43:27 +0000

    Server update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+app.get('/', (req, res) => {
+  res.send('Sonicium Q Backend Running');
+});
+
+module.exports = app;
