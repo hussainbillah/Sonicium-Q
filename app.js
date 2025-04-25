6ba077885e45671a4f70b156e21a97ceca2013fd
hussainbillah <wnnbdonline@gmail.com>
2025-04-25 19:51:33 +0000

    Replit update h

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = app.use(express.static(path.join(__dirname, 'public')));ess');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
