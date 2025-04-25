1970ae9727a7012c206a85b18e63365fe0619572
hussainbillah <wnnbdonline@gmail.com>
2025-04-25 19:06:54 +0000

    Auth user sign-up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
-// routes/authRoutes.js
+
 const express = require('express');
 const router = express.Router();
+const { signup, login } = require('../controllers/authController');
 
-router.get('/test', (req, res) => {
-  res.send('Auth route is working!');
-});
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
