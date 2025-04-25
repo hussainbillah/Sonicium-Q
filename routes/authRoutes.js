5b7dd4af1d46376518ce221ae95f712de1499d60
hussainbillah <wnnbdonline@gmail.com>
2025-04-25 18:26:28 +0000

    Authroute update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,9 @@
+// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+
+router.get('/test', (req, res) => {
+  res.send('Auth route is working!');
+});
+
 module.exports = router;
