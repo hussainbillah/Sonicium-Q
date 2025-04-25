e71152fd228a38ba4d5acfdcd1d33870604b490f
hussainbillah <wnnbdonline@gmail.com>
2025-04-25 19:37:43 +0000

    App js update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
